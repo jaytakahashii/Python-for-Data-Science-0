2055301f5334d62a3738623f21e71c291436572f
Jay Takahashi <jtakahas@student.42.fr>
2025-10-22 14:55:44 +0200

    feat: completed ex01

---
+++ ex01/format_ft_time.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+import time
+
+print('-- - Using time module ---')
+timestamp = time.time()
+print(f"Seconds since January 1, 1970: {timestamp:.4f} or {timestamp:.2e} in scientific notation")
+print(time.strftime("%b %d %Y", time.localtime(timestamp)))
+
+print('\n-- - Using datetime module ---')
+now = datetime.now()
+timestamp = now.timestamp()
+print(f"Seconds since January 1, 1970: {timestamp:.4f} or {timestamp:.2e} in scientific notation")
+print(now.strftime("%b %d %Y"))
