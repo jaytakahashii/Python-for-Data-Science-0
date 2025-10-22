8ab8ec4e3f05e3321c45e6c239beeedc90a84ceb
Jay Takahashi <jtakahas@student.42.fr>
2025-10-22 16:20:31 +0200

    feat: completed ex02

---
+++ ex02/find_ft_type.py
@@ -0,0 +1,17 @@
+def all_thing_is_obj(object: any) -> int:
+  type_map = {
+    list: "List",
+    tuple: "Tuple",
+    set: "Set",
+    dict: "Dict",
+    str: f"{object} is in the kitchen",
+  }
+
+  t = type(object)
+
+  if t in type_map:
+    print(type_map[t], ":", t)
+  else:
+    print("Type not found")
+
+  return 42
