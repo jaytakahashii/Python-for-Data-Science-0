4cf85b1d332f3ca9d7a9ca3f2cf7a2e120eb769f
Jay Takahashi <jtakahas@student.42.fr>
2025-10-24 18:28:15 +0200

    feat: create building.py, Almost OK

---
+++ ex05/building.py
@@ -0,0 +1,52 @@
+import sys
+import string
+
+
+def countChars(text: str) -> dict:
+    # count types of characters in a string
+    details = {
+        "upper": 0,
+        "lower": 0,
+        "punctuation": 0,
+        "spaces": 0,
+        "digits": 0,
+    }
+    for char in text:
+        if char.isupper():
+            details["upper"] += 1
+        elif char.islower():
+            details["lower"] += 1
+        elif char in string.punctuation:
+            details["punctuation"] += 1
+        elif char.isdigit():
+            details["digits"] += 1
+        elif char.isspace():
+            details["spaces"] += 1
+    return details
+
+
+def main():
+    try:
+        if len(sys.argv) > 2:
+            raise AssertionError("more than one argument is provided")
+        if len(sys.argv) == 1:
+            print("What is the text to count?")
+            text: str = sys.stdin.read()
+        else:
+            text: str = sys.argv[1]
+        counts: dict = countChars(text)
+        print(f"The text contains {len(text)} characters:")
+        print(f"{counts['upper']} upper letters")
+        print(f"{counts['lower']} lower letters")
+        print(f"{counts['punctuation']} punctuation marks")
+        print(f"{counts['spaces']} spaces")
+        print(f"{counts['digits']} digits")
+
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
