2191920c3c0011c278696cffaeb653b59c755b10
Jay Takahashi <jtakahas@student.42.fr>
2025-10-24 16:52:22 +0200

    feat: smarter

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,28 +1,22 @@
 import sys
 
-def EvenOrOdd(num: int) -> bool:
-  Even: bool = True
-  Odd: bool = False
+def EvenOrOdd(num: int) -> str:
   if num % 2 == 0:
-    return Even
+    return "Even"
   else:
-    return Odd
+    return "Odd"
 
 if __name__ == "__main__":
-  if len(sys.argv) == 1:
-    sys.exit(0)
-
-  if len(sys.argv) != 2:
-    print("AssertionError: more than one argument is provided")
-    sys.exit(1)
-
   try:
+    if len(sys.argv) == 1:
+      exit()
+
+    if len(sys.argv) > 2:
+      raise AssertionError("more than one argument is provided")
+
     number = int(sys.argv[1])
+    print(f"I'm {EvenOrOdd(number)}.")
   except ValueError:
     print("AssertionError: argument is not an integer")
-    sys.exit(1)
-
-  if EvenOrOdd(number):
-    print("I'm Even.")
-  else:
-    print("I'm Odd.")
+  except AssertionError as e:
+    print(f"AssertionError: {e}")
