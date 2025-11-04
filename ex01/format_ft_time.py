84556c54b089779b068cbdb7eac744e29e50390c
Jay Takahashi <jtakahas@student.42.fr>
2025-11-04 15:29:27 +0100

    fix: format error

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -4,14 +4,16 @@ import time
 print('-- - Using time module ---')
 timestamp = time.time()
 print(
-    f"Seconds since January 1, 1970: {timestamp: .4f} \
-        or {timestamp: .2e} in scientific notation")
+    f"Seconds since January 1, 1970: {timestamp:,.4f}"
+    f" or {timestamp:.2e} in scientific notation"
+)
 print(time.strftime("%b %d %Y", time.localtime(timestamp)))
 
 print('\n-- - Using datetime module ---')
 now = datetime.now()
 timestamp = now.timestamp()
 print(
-    f"Seconds since January 1, 1970: {timestamp: .4f} \
-        or {timestamp: .2e} in scientific notation")
+    f"Seconds since January 1, 1970: {timestamp:,.4f}"
+    f" or {timestamp:.2e} in scientific notation"
+)
 print(now.strftime("%b %d %Y"))
